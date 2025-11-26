eca814a1c70a4d02ba2628fb3806675d69f1a351
Ayush Chugani <ayushchugani@gmail.com>
2025-11-26 10:48:12 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,6 +1,7 @@
-def add(a,b):
-  return a + b
+def add(a, b):
+    return a + b
+import sys
 if __name__ == "__main__":
-  x = 10
-  y = 20
-  print("Sum : ",add(x,y))
+    num1 = int(sys.argv[1])
+    num2 = int(sys.argv[2])
+    print("Sum:", add(num1, num2))
